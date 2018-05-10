3ce55c9bb94bb940b9b5a50a050a0544a5f4b6e8
Jader Martins <jadermcs94@gmail.com>
2018-05-10 12:08:51 -0300

    extends for parallel or sequential collections

--- src/main/scala/MapRed.scala
+++ src/main/scala/MapRed.scala
@@ -1,26 +1,28 @@
 import scala.collection.parallel.immutable.ParVector
 import scala.collection.parallel.ParMap
+import scala.collection.GenSeq
+import scala.collection.GenMap
 import scala.io.Source
 
 object MapRed {
   private val stopWords = Source.fromResource("stop_words.txt").getLines.toSeq
   def isStopWord(word: String): Boolean = stopWords.contains(word)
 
-  def words(lines: ParVector[String]): ParVector[String] =
+  def words(lines: GenSeq[String]): GenSeq[String] =
     lines.flatMap(_.split(' ')).map(_.toLowerCase)
       .map(_.replaceAll("""[^a-z]""", ""))
       .filter(w => !isStopWord(w) && w != "")
 
-  def map(seq: ParVector[String]): ParVector[(String, Int)] =
+  def mapper(seq: GenSeq[String]): GenSeq[(String, Int)] =
     seq.map(_ -> 1)
 
-  def reduce(seq: ParVector[(String, Int)]): ParMap[String, Int] =
-    seq.groupBy(_._1).mapValues(_.map(_._2).reduce(_+_))
+  def reducer(seq: GenSeq[(String, Int)]): GenMap[String, Int] =
+    seq.groupBy(_._1).mapValues(_.map(_._2).sum)
 
   def main(args: Array[String]): Unit = {
     val fileIterator = Source.fromResource("SHAKESPEARE.txt").getLines
     val lines = fileIterator.toVector.par
-    val result = reduce(map(words(lines)))
+    val result = reducer(mapper(words(lines)))
     println(result.seq.toList.sortBy(-_._2).take(100))
   }
 }
